5eed3900252f6e200a5b0f6a53c3391b9131288e
pierrejacksoo <luciferpeklo@infopeklo.cz>
2025-02-26 12:15:56 +0100

    Update Hash.py

--- Hash.py
+++ Hash.py
@@ -1,7 +1,7 @@
 import hashlib
 
 # Define the weak password
-weak_password = "password123"  # Example of a weak password
+weak_password = "987654321"  # Example of a weak password
 
 # Generate the SHA-256 hash of the password
 password_hash = hashlib.sha256(weak_password.encode()).hexdigest()
